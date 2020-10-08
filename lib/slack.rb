0906103a7d79ed9d43a4dd04081e768c1452901a
Alice D <alicedilanchian@gmail.com>
2020-10-08 17:58:08 -0500

    Wave 2 command skeleton added in

--- lib/slack.rb
+++ lib/slack.rb
@@ -17,7 +17,7 @@ def main
   puts "There are #{total_users} users in the Ada Slack workspace and #{total_channels} channels in the Ada Slack workspace."
 
   until command == "quit" || command == "q"
-    print "What do you want to do? Here are the options: list users, list channels, or quit => "
+    print "What do you want to do? Here are the options: list users, list channels, select user, select channel, details, or quit => "
     command = gets.chomp.downcase
     if command == "list users"
       #ap workspace.get_users
@@ -25,6 +25,29 @@ def main
     elsif command == "list channels"
       #ap workspace.get_channels
       tp workspace.get_channels, :name, :topic, :member_count, :slack_id
+    elsif command == "select user"
+      puts "Do you want to input a username or a slack ID?"
+
+      answer = gets.chomp.downcase
+
+      until answer == "quit"
+
+        if answer == "username"
+          puts "calling username method" #workspace.select_username
+          answer = "quit"
+        elsif answer == "slack id"
+          puts "calling slack id method" # workspace.select_slack_id
+          answer = "quit"
+        else
+          "#{answer} is not a valid option. Enter username, slack id, or quit."
+        end
+
+      end
+
+    elsif command == "select channel"
+      # workspace.select_channel
+    elsif command == "details"
+      # workspace.details
     else
       puts "That is not a valid command. Please provide a command from the provided list."
     end
